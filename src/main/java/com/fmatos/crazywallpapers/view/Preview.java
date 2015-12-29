7d26481427309c5396e3ae4e3fe59becde232a9b
Fabio de Matos <fdematos@despegar.com>
2015-12-28 23:42:31 -0300

    Initial commit

---
+++ src/main/java/com/fmatos/crazywallpapers/view/Preview.java
@@ -0,0 +1,15 @@
+package com.fmatos.crazywallpapers.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.fmatos.crazywallpapers.R;
+
+public class Preview extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_preview);
+	}
+}
